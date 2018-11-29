4e0379a73d3404aa07d34cc8975a714716711d85
zxl1314123 <652480469@qq.com>
2018-11-29 09:32:19 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:ZXL
+ */
+
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交111");
+    }
+}
